a255e788517f7605176e0b6566d58543c0f561e7
monkeychen528 <53263222+monkeychen528@users.noreply.github.com>
2019-08-14 20:45:26 +0800

    php for迴圈

---
+++ bar_layout.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+    <table>
+        <?php for($i=1;$i<10;$i++){ ?>
+            <tr>
+                <?php for($k=1;$k<10;$k++) {?>
+                <td><?php echo $k*$i ?></td>
+                <?php }?>
+            </tr>
+        <?php }?>
+    </table>
+</body>
+</html>
